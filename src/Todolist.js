acd9e9dd46c6c213a1f3009ef72879918c99ae56
ilhemboubaker <ilhem.boubaker580@gmail.com>
2023-12-11 17:24:57 +0100

    Initial commit

---
+++ src/Todolist.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    // Simuler le chargement initial des tâches depuis une source de données
+    const initialTasks = localStorage.getItem('tasks');
+    if (initialTasks) {
+      setTasks(JSON.parse(initialTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Sauvegarder les tâches dans le stockage local à chaque changement
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { id: Date.now(), text: newTask, completed: false }]);
+      setNewTask('');
+    }
+  };
+
+  const toggleTask = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTask(task.id)}
+            />
+            <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+              {task.text}
+            </span>
+            <button onClick={() => deleteTask(task.id)}>Supprimer</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Nouvelle tâche"
+        />
+        <button onClick={addTask}>Ajouter</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
